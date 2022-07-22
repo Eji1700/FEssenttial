576834439ee15220c0919933d458ab67bb15d447
Eji1700 <regulus87@yahoo.com>
2022-07-22 13:27:40 -0700

    screwing with smal issues.  basic templates up

---
+++ Chapter 4/MySolutionManual/src/MyProject/Customer.fs
@@ -0,0 +1,17 @@
+module MyApplication.Customer // Namespace.Module
+open System
+
+type Customer = {
+    Name : string
+}
+
+module Domain =
+    let create (name:string) =
+    { Name = name }
+    
+module Db =
+    open System.IO
+
+    let save (customer:Customer) =
+    // Imagine this talks to a database
+    ()
---
+++ Chapter 4/MySolutionManual/tests/MyProjectTests/CustomerTests.fs
@@ -0,0 +1,14 @@
+namespace MyProjectTests
+
+open System
+open Xunit
+
+module ``I can group my tests in a module and run`` =
+
+    [<Fact>]
+    let ``My first test`` () =
+        Assert.True(true)
+
+    [<Fact>]
+    let ``My second test`` () =
+        Assert.True(true)
