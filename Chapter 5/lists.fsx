cea7aa9bf1000e71237cc1ece775f06776fd5e0c
Eji1700 <regulus87@yahoo.com>
2022-08-14 02:51:31 -0700

    fold stuff done, skimmed most of the rest up toe the practical exampel

--- Chapter 5/lists.fsx
+++ Chapter 5/lists.fsx
@@ -9,3 +9,13 @@ let items =
 let sum items = 
     items
     |> List.sumBy (fun (q,p) -> decimal q * p)
+
+[ 1 .. 10 ]
+|> List.fold (fun acc v -> acc + v ) 0
+
+let getTotal items =
+    items 
+    |> List.fold (fun acc (q, p) -> acc + decimal q * p) 0M
+
+let total = getTotal items
+
