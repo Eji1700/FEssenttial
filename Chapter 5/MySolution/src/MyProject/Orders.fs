fc849d0f77e5aa2de73962d0e837c0aaa8bb5c9a
Eji1700 <regulus87@yahoo.com>
2022-08-19 12:19:39 -0700

    Did testing and mocked up additem.

--- Chapter 5/MySolution/src/MyProject/Orders.fs
+++ Chapter 5/MySolution/src/MyProject/Orders.fs
@@ -12,14 +12,13 @@ type Order = {
 
 module Domain =
     let addItem item order =
-        let items = item::order.Items
+        let items = 
+            item::order.Items
+            |> List.groupBy (fun i -> i.ProductId)
+            |> List.map(fun (id, items) -> { ProductId = id; Quantity = items |> List.sumBy ( fun i -> i.Quantity)})
+            |> List.sortBy (fun i -> i.ProductId)
         { order with Items = items}
 
     let order = { Id = 1; Items = [ { ProductId = 1; Quantity = 1 } ] }
     let newItemExistingProduct = { ProductId = 1; Quantity = 1 }
     let newItemNewProduct = { ProductId = 2; Quantity = 2 }
-
-    addItem newItemNewProduct order =
-        { Id = 1; Items = [ { ProductId = 1; Quantity = 1 };{ ProductId = 2; Quantity = 2 } ] }
-    addItem newItemExistingProduct order =
-        { Id = 1; Items = [ { ProductId = 1; Quantity = 2 } ] }
