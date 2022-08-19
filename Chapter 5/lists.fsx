258ca11bac3ce26a841716bdb7c5a66d229d3a66
Eji1700 <regulus87@yahoo.com>
2022-08-18 20:09:12 -0700

    list module, chapter 5

--- Chapter 5/lists.fsx
+++ Chapter 5/lists.fsx
@@ -19,3 +19,35 @@ let getTotal items =
 
 let total = getTotal items
 
+let myList = [1;2;3;4;5;7;6;5;4;3]
+
+let gbResult = myList |> List.groupBy id
+
+let unique items =
+    items
+    |> List.groupBy id
+    |> List.map (fun (i,_) -> i)
+
+let unResult = unique myList
+
+let uniqueSet items = 
+    items |> Set.ofList
+
+let setResult = uniqueSet myList
+
+let nums = [1..10]
+
+nums
+|> List.filter (fun v -> v % 2 = 1)
+|> List.map (fun v -> v * v)
+|> List.sum
+
+match nums with 
+| [] -> 0
+| items -> 
+    items
+    |> List.reduce( fun acc v -> acc + if v % 2 = 1 then (v * v) else 0)
+
+nums
+|> List.sumBy (fun v -> if v % 2 = 1 then (v * v) else 0)
+
