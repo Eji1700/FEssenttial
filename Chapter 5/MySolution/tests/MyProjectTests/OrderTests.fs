bcec7e8b8c78891b602d6044199affd6efec6975
Eji1700 <regulus87@yahoo.com>
2022-08-19 12:26:22 -0700

    added first test and removed reference to old library.fs file

--- Chapter 5/MySolution/tests/MyProjectTests/OrderTests.fs
+++ Chapter 5/MySolution/tests/MyProjectTests/OrderTests.fs
@@ -1,1 +1,13 @@
-namespace MyProject.Orders
+namespace OrderTests
+open MyProject.Orders
+open MyProject.Orders.Domain
+open Xunit
+open FsUnit
+
+module ``Add item to order`` =
+    [<Fact>]
+    let ``when product does not exist in empty order`` () =
+        let myEmptyOrder = { Id = 1; Items = [] }
+        let expected = { Id = 1; Items = [ { ProductId =1; Quantity = 3} ] }
+        let actual = myEmptyOrder |> addItem { ProductId = 1; Quantity = 3 }
+        actual |> should equal expected
