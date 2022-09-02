81af73d6e055a721aaf564baac2764626112b022
Eji1700 <regulus87@yahoo.com>
2022-09-02 11:20:14 -0700

    created addItems function

--- Chapter 5/MySolution/src/MyProject/Orders.fs
+++ Chapter 5/MySolution/src/MyProject/Orders.fs
@@ -19,6 +19,15 @@ module Domain =
             |> List.sortBy (fun i -> i.ProductId)
         { order with Items = items}
 
-    let order = { Id = 1; Items = [ { ProductId = 1; Quantity = 1 } ] }
-    let newItemExistingProduct = { ProductId = 1; Quantity = 1 }
-    let newItemNewProduct = { ProductId = 2; Quantity = 2 }
+    let addItems newItems order =  
+        let items =
+            newItems @ order.Items
+            |> List.groupBy ( fun i -> i.ProductId )
+            |> List.map (fun ( id, items ) ->
+                {   ProductId = id
+                    Quantity =
+                        items
+                        |> List.sumBy(fun i ->
+                            i.Quantity)})
+            |> List.sortBy(fun i -> i.ProductId)
+        { order with Items = items}
