3f6fc33766f30234e3dee09dabd48b6e71722596
Eji1700 <regulus87@yahoo.com>
2022-09-13 22:09:09 -0700

    added try catch error handling

--- Chapter 6/Program.fs
+++ Chapter 6/Program.fs
@@ -2,15 +2,22 @@
 open System.IO
 
 let readFile path =
-    seq {
-        use reader = new StreamReader(File.OpenRead(path))
-        while not reader.EndOfStream do
-            reader.ReadLine()
-    }
+    try
+        seq {
+            use reader = new StreamReader(File.OpenRead(path))
+            while not reader.EndOfStream do
+                reader.ReadLine()
+        } |> Ok
+    with
+    | ex -> Error ex
+
+let import path =
+    match path |> readFile with
+    |> Ok data -> data |> Seq.iter(fun x -> printfn "%A" x)
+    |> Error ex -> printfn "Error: %A" ex.Message
 
 [<EntryPoint>]
 let main argv =
     let path = Path.Combine(__SOURCE_DIRECTORY__, "resources", "customers.csv")
-    let data = readFile path
-    data |> Seq.iter (fun x -> printfn "%s" x)
+    |> import
     0
