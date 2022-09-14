8f908b56201f604142399d856018245feff70080
Eji1700 <regulus87@yahoo.com>
2022-09-13 22:05:30 -0700

    created readFile function to read data from a file

--- Chapter 6/Program.fs
+++ Chapter 6/Program.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+
+let readFile path =
+    seq {
+        use reader = new StreamReader(File.OpenRead(path))
+        while not reader.EndOfStream do
+            reader.ReadLine()
+    }
+
+[<EntryPoint>]
+let main argv =
+    let path = Path.Combine(__SOURCE_DIRECTORY__, "resources", "customers.csv")
+    let data = readFile path
+    data |> Seq.iter (fun x -> printfn "%s" x)
+    0
