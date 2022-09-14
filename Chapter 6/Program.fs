2db75ac9b2f730679c6b3af9d937cd831acc0e82
Eji1700 <regulus87@yahoo.com>
2022-09-13 22:21:41 -0700

    created parse function

--- Chapter 6/Program.fs
+++ Chapter 6/Program.fs
@@ -1,6 +1,33 @@
 ﻿open System
 open System.IO
 
+type Customer = {
+    CustomerId : string
+    Email : string
+    IsEligible : string
+    IsRegistered : string
+    DateRegistered : string
+    Discount : string
+}
+
+let parse (data:string seq) =
+    data
+    |> Seq.skip 1
+    |> Seq.map( fun line ->
+        match line.Split('|') with 
+        | [| customerId; email; eligible; registered; dateRegistered; discount |] ->
+            Some {
+                CustomerId = customerId
+                Email = email
+                IsEligible = eligible
+                IsRegistered = registered
+                DateRegistered = dateRegistered
+                Discount = discount
+            }
+        | _ -> None
+    )
+    |> Seq.choose id
+
 let readFile path =
     try
         File.ReadLines(path)
@@ -10,7 +37,7 @@ let readFile path =
 
 let import path =
     match path |> readFile with
-    | Ok data -> data |> Seq.iter(fun x -> printfn "%A" x)
+    | Ok data -> data |> parse |> Seq.iter(fun x -> printfn "%A" x)
     | Error ex -> printfn "Error: %A" ex.Message
 
 [<EntryPoint>]
